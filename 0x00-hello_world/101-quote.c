e137f5c13913fca6cdd67ec4f83d9b5e3ceff755
Yisehak Kassahun <yisehakschool@gmail.com>
2024-06-20 11:57:20 -0700

    Add C program to print a quote to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo '#include <unistd.h>'$'\n\nint main(void)\n{\n    write(2, "and that piece of art is useful\\" - Dora Korpar, 2015-10-19\n", 59);\n    return (1);\n}' > 101-quote.c && gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -o quote 101-quote.
