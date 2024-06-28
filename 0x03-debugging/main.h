9fb7c0ab30eee73c12da2366dfb97ff043034954
Yisehak Kassahun <yisehakschool@gmail.com>
2024-06-28 10:03:51 -0700

    Fix print_remaining_days to handle leap years correctly

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,13 +6,3 @@
 int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
-
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
-
-#endif /* MAIN_H */
