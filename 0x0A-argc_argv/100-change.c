1c569e60d77e6e3adf83a3ba3821e40baf201a88
YisehakKEK <yisehakschool@gmail.com>
2025-04-10 18:18:11 +0300

    final

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,33 +10,33 @@
  */
 int main(int argc, char *argv[])
 {
-    int cents, coins = 0;
-    int denominations[] = {25, 10, 5, 2, 1};
-    int i;
+int cents, coins = 0;
+int denominations[] = {25, 10, 5, 2, 1};
+int i;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
 
-    cents = atoi(argv[1]);
+cents = atoi(argv[1]);
 
-    if (cents < 0)
-    {
-        printf("0\n");
-        return (0);
-    }
+if (cents < 0)
+{
+printf("0\n");
+return (0);
+}
 
-    for (i = 0; i < 5; i++)
-    {
-        while (cents >= denominations[i])
-        {
-            cents -= denominations[i];
-            coins++;
-        }
-    }
+for (i = 0; i < 5; i++)
+{
+while (cents >= denominations[i])
+{
+cents -= denominations[i];
+coins++;
+}
+}
 
-    printf("%d\n", coins);
-    return (0);
+printf("%d\n", coins);
+return (0);
 }
